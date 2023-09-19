03be70980df4540c31ba90a4a035b7288d7091ff
Mike Bwisa <mkbwisam14@gmail.com>
2023-09-19 10:14:32 +0300

    1stcommit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOps!!! Seems I'm hacked</title>
+</head>
+<body>
+    <img align="center" src="img/zac-efron-not-mad-just-disappointed-wn8fhcqbpq1kgmeu"></img>
+</body>
+</html>
